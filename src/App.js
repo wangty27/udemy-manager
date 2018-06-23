ded588b3ad20a629f4cad0d66773133d0986623f
Terry Wang <terrywang@tianyu.ninja>
2018-06-23 17:25:14 -0400

    react-native-router-flux setup

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
   componentWillMount() {
@@ -30,7 +31,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
