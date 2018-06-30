3c3ebb1cf7fd841f15c6687c35220810a405acc8
Terry Wang <terrywang@tianyu.ninja>
2018-06-30 02:05:39 -0400

    employee editform

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
   return (
@@ -21,6 +22,7 @@ const RouterComponent = () => {
             initial
           />
           <Scene key='employeeCreate' component={EmployeeCreate} title='Add Employee' />
+          <Scene key='employeeEdit' component={EmployeeEdit} title='Edit Employee' />
         </Scene>
       </Scene>
     </Router>
---
+++ src/components/EmployeeEdit.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection, Button } from './common';
+
+class EmployeeEdit extends Component {
+  componentWillMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
+  onSaveButtonPress() {
+    const { name, phone, shift } = this.props;
+    console.log(name, phone, shift);
+  }
+
+  render() {
+    return (
+      <Card>
+        <EmployeeForm />
+        <CardSection>
+          <Button onPress={this.onSaveButtonPress.bind(this)}>Save</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
